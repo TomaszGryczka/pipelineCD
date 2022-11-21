d04cc436740cb7a1afc630888f74500064a69c43
TomaszGryczka <tumek001@gmail.com>
2022-11-21 18:59:05 +0100

    say hello :)

--- src/main/java/com/github/tomaszgryczka/pipelinecd/BasicController.java
+++ src/main/java/com/github/tomaszgryczka/pipelinecd/BasicController.java
@@ -1,13 +1,14 @@
 package com.github.tomaszgryczka.pipelinecd;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicController {
 
-    @GetMapping("/helloworld")
-    public String helloWorld() {
-        return "hello azure";
+    @RequestMapping("/")
+    String sayHello() {
+        return "Hello World!";
     }
 }
